080c509ef2606c6a1409ee46f11a0c85b701f657
David Rosenbaum <DaveR1226@gmail.com>
2023-08-02 14:18:15 -0500

    Moved ArrayUtils.php to new src directory

---
+++ src/ArrayUtils.php
@@ -0,0 +1,25 @@
+<?php
+
+class ArrayUtils
+{
+
+    private function __construct()
+    {
+    }
+
+    public static function mergeArrays(array $array1, array $array2): array
+    {
+        // Could just be done like this
+        // return array_merge($array1, $array2);
+
+        // Implement without PHP built in array code
+        $mergedArray = [];
+        foreach ($array1 as $key => $value) {
+            $mergedArray[$key] = $value;
+        }
+        foreach ($array2 as $key => $value) {
+            $mergedArray[$key] = $value;
+        }
+        return $mergedArray;
+    }
+}
